2430719e250f666abc658534004a7f8863f1d6c1
424004242-cell <424004242@ntc.edu.ph>
2026-05-24 21:58:18 +0800

    Update LoginForm.Designer.vb

--- StudentResourceTracker code/LoginForm.Designer.vb
+++ StudentResourceTracker code/LoginForm.Designer.vb
@@ -14,12 +14,8 @@ Partial Class LoginForm
         End Try
     End Sub
 
-    'Required by the Windows Form Designer
     Private components As System.ComponentModel.IContainer
 
-    'NOTE: The following procedure is required by the Windows Form Designer
-    'It can be modified using the Windows Form Designer.  
-    'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.SuspendLayout()

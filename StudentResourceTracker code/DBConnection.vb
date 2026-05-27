06cf481ceadfb416985a04499320440356eb3a6d
424004242-cell <424004242@ntc.edu.ph>
2026-05-27 23:32:30 +0800

    Delete StudentResourceTracker code directory

--- StudentResourceTracker code/DBConnection.vb
+++
@@ -1,9 +0,0 @@
-﻿Imports System.Data.SqlClient
-
-Public Class DBConnection
-
-    Public Shared con As New SqlConnection(
-        "Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=StudentResourceTracker;Integrated Security=True"
-    )
-
-End Class
